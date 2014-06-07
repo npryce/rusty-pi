51ccf5109108c1f98a4f93a3132cc862a568238d
Nat Pryce <sw@natpryce.com>
2014-06-07 01:11:45 +0100

    a program to experiment with signal handling

---
+++ src/signals.rs
@@ -0,0 +1,31 @@
+
+extern crate green;
+extern crate rustuv;
+
+use std::io::signal::{Listener, Interrupt};
+
+#[start]
+fn start(argc: int, argv: **u8) -> int {
+    green::start(argc, argv, rustuv::event_loop, main)
+}
+
+fn main() {
+    let mut listener = Listener::new();
+    
+    match listener.register(Interrupt) {
+        Err(e) => {
+            println!("failed to register for interrupt signal: {}", e);
+            fail!();
+        },
+        _ => ()
+    }
+    
+    loop {
+        match listener.rx.recv() {
+            Interrupt => {
+                fail!("interrupted");
+            },
+            _ => ()
+        }
+    }
+}
