f7d969cc930aec34296aa11c5a744a24db7f7550
Nat Pryce <sw@natpryce.com>
2014-07-03 23:49:48 +0100

    ownership example for File

--- src/file-ownership-ok.rs
+++ src/file-ownership-ok.rs
@@ -4,12 +4,12 @@ use std::io::{File,IoResult};
 fn main() {
     let mut f = File::open(&Path::new("hello.txt"))
         .ok().expect("could not open file");
-    use_file(f)
+    use_file(&mut f)
         .ok().expect("could not use file");
     f.flush()
         .ok().expect("could not flush file");
 }
 
-fn use_file(f: File) -> IoResult<()> {
+fn use_file(f: &mut File) -> IoResult<()> {
     f.write_str("hello")
 }
