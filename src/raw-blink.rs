729c1c3999f7282273ef215c41d545f11a4c8e5d
Nat Pryce <sw@natpryce.com>
2014-07-05 00:01:13 +0100

    neaten up raw-blink to make use of write! macro a bit less awkward

--- src/raw-blink.rs
+++ src/raw-blink.rs
@@ -1,18 +1,25 @@
-use std::io::File;
+use std::fmt::Show;
+use std::io::{File, IoResult};
 use std::io::timer::sleep;
 
+fn write_to<T:Show>(path: &str, value: T) -> IoResult<()> {
+    let mut f = try!(File::create(&Path::new(path)));
+    write!(f, "{}", value)
+}
+
 fn main() {
-    (write!(File::create(&Path::new("/sys/class/gpio/export")), "{}", 18u))
-        .ok().expect("failed to export pin");
+    write_to("/sys/class/gpio/export", 18u)
+        .unwrap();
+
+    write_to("/sys/class/gpio/gpio18/direction", "out")
+        .unwrap();
     
-    let mut pin_file = File::create(&Path::new("/sys/class/gpio/gpio18/value"));
-    
-    for i in range(1u,21u) {
-        (write!(pin_file, "{}", i%2))
-            .ok().expect("failed to write pin value");
+    for i in range(1u, 21) {
+        write_to("/sys/class/gpio/gpio18/value", i%2)
+            .unwrap();
         sleep(500);
     }
     
-    (write!(File::create(&Path::new("/sys/class/gpio/unexport")), "{}", 18u))
-        .ok().expect("failed to unexport pin");
+    write_to("/sys/class/gpio/unexport", 18u)
+        .unwrap();
 }
