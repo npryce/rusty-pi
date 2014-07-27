492896e7f5bbb71820c1a6a8018d260240b72380
Nat Pryce <sw@natpryce.com>
2014-07-27 21:27:09 +0100

    extend eventfd-example to demonstrate interaction with epoll

--- src/eventfd-example.rs
+++ src/eventfd-example.rs
@@ -1,5 +1,6 @@
 
 use pi::eventfd::semaphore;
+use pi::epoll::{IoSelector,IN};
 use std::io::timer::sleep;
 
 mod pi;
@@ -8,13 +9,21 @@ fn main() {
     let (send, recv) = semaphore(0).unwrap();
     
     spawn(proc() {
+        println!("SENDER: sleeping...");
         sleep(1000);
-        println!("sending signal");
+        println!("SENDER: awake - sending signal");
         send.signal().unwrap();
     });
     
     spawn(proc() {
+        let mut s = IoSelector::create().unwrap();
+        s.add(&recv, IN, 0).unwrap();
+        
+        println!("RECEIVER: waiting for signal...");
+        s.wait().unwrap();
+        
+        println!("RECEIVER: receiving signal");
         recv.recv().unwrap();
-        println!("signal received");
+        println!("RECEIVER: signal received");
     });
 }
